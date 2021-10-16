38101686f55c78aff3056fda829b9cd2b05d7724
anhpv27 <vietanhp@reactplus.com>
2021-10-16 10:30:30 +0700

    update: slick

--- js/index.js
+++ js/index.js
@@ -14,7 +14,7 @@ $(document).ready(function () {
         settings: {
           slidesToShow: 2,
           slidesToScroll: 1,
-          arrows: true,
+          arrows: false,
         },
       },
       {
